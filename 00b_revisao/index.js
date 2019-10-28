5c0f3e620e726aaa199ee3400eeaf6e8d464111b
ema_maia@hotmail.com <emamaia@hotmail.com>
2019-10-28 13:31:03 -0300

    first commit

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -1,14 +1,34 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+const body = document.body;
+console.log(body);
+const botao = document.getElementById('trocaCor');
+
+function geraInteiro(){
+    const n=parseInt(Math.random() * 255);
+    return n;   
+}
+
+botao.addEventListener('click', ()=>{
+    const r = geraInteiro();
+    const g = geraInteiro();
+    const b = geraInteiro();
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+});
+
+
+
+
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
-// [] arrow function
+// [x] remover repetição de código
+// [x] anonymous function
+// [x] arrow function
 // [] template string
