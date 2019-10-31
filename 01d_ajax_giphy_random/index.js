334f879490d56989f04c05af57a8853c575a71aa
ema_maia@hotmail.com <emamaia@hotmail.com>
2019-10-31 09:06:38 -0300

    Atualiza exercícios

--- 01d_ajax_giphy_random/index.js
+++ 01d_ajax_giphy_random/index.js
@@ -5,3 +5,35 @@
 // [] transformar o dado recebido
 // [] apresentar <img>
 // [] usar async/await, then(), .catch(), arrow function
+
+// const img = document.getElementById('imagem')
+
+const request = new XMLHttpRequest();
+request.onreadystatechange = mostraGif;
+
+const url = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=3&offset=0`
+request.open('GET', url);
+request.send();
+
+
+function mostraGif() {
+
+    if(request.readyState === 4 && request.status === 200){            
+        const response = request.response;             
+        const json= JSON.parse(response);
+        console.log(json);
+        
+        // const data= json.data[0].images.original.url;        
+        const body = document.body;
+        body.innerHTML = `
+          <img src=${json.data[0].images.original.url}>
+          <img src=${json.data[1].images.original.url}>
+          <img src=${json.data[2].images.original.url}>        
+        `
+            // img.src= data;        
+    } else {
+                
+    }
+        
+}
+
